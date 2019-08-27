5c8ed1c8fefa1a466df4818f7c593ade21e10822
Bogdan Kozlovskyi <kozlovskyi.bohdan@gmail.com>
2019-08-27 11:23:23 +0300

    Task 3 with 2 options has been added

---
+++ fizz-buzz_V1.py
@@ -0,0 +1,18 @@
+fizz = int(input("Введите число Fizz: "))
+buzz = int(input("Введите число Buzz: "))
+number = int(input("Введите последнее число: "))
+
+string_list = []
+
+for i in range(1, number+1):
+    if i%fizz == 0 and i%buzz == 0:
+        i = "FB"
+    elif  i%fizz == 0:
+        i = "F"
+    elif i%buzz == 0:
+        i = "B"
+    else:
+        i
+    string_list.append(i)
+
+print(*string_list)
