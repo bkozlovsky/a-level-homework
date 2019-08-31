e7623f8aa8a272059862e2df2fa01556e0eb00d5
Bogdan Kozlovskyi <kozlovskyi.bohdan@gmail.com>
2019-08-31 15:08:51 +0300

    added hometask 3

---
+++ Домашнее задание №3/fizzbuzz1.py
@@ -0,0 +1,29 @@
+import sys
+
+filename = sys.argv[1]
+
+f = open(filename, 'r')
+
+first_string = list(f)[0].split()
+
+fizz = int(first_string[0])
+buzz = int(first_string[1])
+number = int(first_string[2])
+
+#Initializing list to collect numbers and letters in a single string
+string_list = []
+
+for i in range(1, number+1):
+   if i%fizz == 0 and i%buzz == 0:
+       i = "FB"
+   elif  i%fizz == 0:
+       i = "F"
+   elif i%buzz == 0:
+       i = "B"
+   else:
+       i
+   string_list.append(i)
+
+print(*string_list)
+
+f.close()
