6a92c673642af18090873034c9677928449a2d48
Bogdan Kozlovskyi <kozlovskyi.bohdan@gmail.com>
2019-10-03 12:00:29 +0300

    renamed folders and added hw11

--- Homework #4/fizz-buzz_V2.py
+++
@@ -1,5 +0,0 @@
-fizz = int(input("Введите число Fizz: "))
-buzz = int(input("Введите число Buzz: "))
-number = int(input("Введите последнее число: "))
-
-print(*["FB" if i%fizz == 0 and i%buzz == 0 else "F" if i%fizz == 0 else "B" if i%buzz == 0 else i for i in range(1, number+1)])
